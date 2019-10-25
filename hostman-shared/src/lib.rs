2069c9836408ef6c955f948429d4820509436738
Shalom Yiblet <shalom.yiblet@gmail.com>
2019-10-25 07:59:17 -0700

    finished initial client

--- hostman-shared/src/lib.rs
+++ hostman-shared/src/lib.rs
@@ -3,9 +3,19 @@ extern crate serde_json;
 use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
 
+type Host = String;
+type Ip = String;
+
 #[derive(Serialize, Deserialize, Default, Debug)]
 pub struct Table {
-    pub host_mapping: BTreeMap<String, String>,
+    pub host_mapping: BTreeMap<Host, Ip>,
+    pub current: Option<Current>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Current {
+    pub host: Host,
+    pub ips: Vec<Ip>,
 }
 
 #[cfg(test)]
